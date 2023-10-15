faee59781f8065bd730ae743ec3a5c2c3a8b9296
LOHJC <81831799+LOHJC@users.noreply.github.com>
2023-10-16 01:58:32 +0800

    updated footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,11 @@ import Link from '@mui/material/Link';
 export const Footer = (props) => {
   return (
     <Typography sx={{backgroundColor: props.theme.palette.primary.main, padding: "1%"}} color={props.color} variant="body2" align="center">
-        {'Copyright © Created by '}
+        {'Created by '}
         <Link color="inherit" href={props.link}>
         {props.name}
         </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
+        {/* {new Date().getFullYear()} */}
     </Typography>
   )
 }
